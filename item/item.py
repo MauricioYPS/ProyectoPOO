03626cfceb68de4df9d7333573d33a1b9f13e913
MauricioYPS <yepes060@gmail.com>
2024-11-08 15:46:05 -0500

    Replay funcional

--- item/item.py
+++ item/item.py
@@ -3,12 +3,16 @@
 import pygame
 
 class Item:
+    next_id = 1  # Variable de clase para asignar IDs únicos
+
     def __init__(self, x, y, image):
         self.x = x  # Coordenada x en el mapa
         self.y = y  # Coordenada y en el mapa
         self.image = image
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
         self.collected = False  # Estado del objeto
+        self.id = Item.next_id  # Asignar un ID único al objeto
+        Item.next_id += 1
 
     def draw(self, screen, offset_x, offset_y):
         """Dibuja el objeto en la pantalla con el desplazamiento dado."""
